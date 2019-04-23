b264a083b4ef77b22f129723dcde5c5e0330a7cc
dongmeifeng-2019 <49913383+dongmeifeng-2019@users.noreply.github.com>
2019-04-23 12:15:57 -0400

    Delete bam_estimate.R

--- bam_estimate.R
+++
@@ -1,9 +0,0 @@
-
-bamestimates = bam_estimate(bamdata = bamdatas, bampriors = bampriors,variant = "amhg",iter = 2000, 
-                            chains = 3, meas_error = TRUE, reparam = TRUE)
-bam_output_format = bam_hydrograph(bamestimates)
-
-
-
-
-
